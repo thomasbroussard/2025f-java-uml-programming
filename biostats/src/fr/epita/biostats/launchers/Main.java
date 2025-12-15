b1626bdd00ba80a339725de1de9c14a1b1ea46dc
thomasbroussard <thomas.broussard@gmail.com>
2025-12-15 11:18:56 +0100

    csv file 2

--- biostats/src/fr/epita/biostats/launchers/Main.java
+++ biostats/src/fr/epita/biostats/launchers/Main.java
@@ -2,9 +2,39 @@ package fr.epita.biostats.launchers;
 
 import fr.epita.biostats.datamodel.Person;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Person person = new Person()
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("biostats/biostat(in).csv"));
+
+        List<Person> list = new ArrayList<>();
+
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+
+            //Name,     Sex, Age, Height (in), Weight (lbs)
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            String currentName = parts[0].trim();
+            String sex = parts[1].trim();
+
+            Integer age = Integer.valueOf(parts[2].trim());
+            Integer height = Integer.valueOf(parts[3].trim());
+            Integer weight = Integer.valueOf(parts[4].trim());
+
+            Person currentCustomer = new Person(currentName, sex, age, height, weight);
+            System.out.println(line);
+            list.add(currentCustomer);
+        }
+
+        System.out.println(list.size());
+
     }
 }
